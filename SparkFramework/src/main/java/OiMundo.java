66fff4ecfb0e53d6454c359201616e43a323f9af
Vinícius Miranda de Araújo <vinicius.araujo.1457564@sga.pucminas.br>
2024-03-10 15:25:52 -0300

    Update SparkFramework

---
+++ SparkFramework/src/main/java/OiMundo.java
@@ -0,0 +1,9 @@
+import static spark.Spark.*;
+
+public class OiMundo 
+{
+	public static void main(String[] arg)
+	{
+			get("/oi", (request, response) -> "Oi Mundo!");
+	} // end main
+} // end class
