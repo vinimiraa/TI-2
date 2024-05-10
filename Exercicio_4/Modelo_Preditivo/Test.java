f9b4568a5c2322db21489b51f227b8ada8ff1bcb
Vinícius Miranda de Araújo <130161601+vinimiraa@users.noreply.github.com>
2024-05-10 15:01:46 -0300

    Update Test.java

--- Exercicio_4/Modelo_Preditivo/Test.java
+++ Exercicio_4/Modelo_Preditivo/Test.java
@@ -8,11 +8,11 @@ public class Test
 	{
 		String body = "{\"Inputs\": {\"input1\": [{\"senha\": \"kzde5577\",\"special character\": 0,\"uppercase\": 0,\">=8\": 1,\"number\": 1,\"strength\": 1}]},\"GlobalParameters\": {}}";
 
-		String url = "https://ussouthcentral.services.azureml.net/workspaces/7f2ec8556b8f453681ccc1fca01369a8/services/756e95b5ed3d46478656395e2092dd85/execute?api-version=2.0&format=swagger";
+		String url = "MINHA URL";
 		
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest req = HttpRequest.newBuilder()
-						.uri(URI.create(url)).header("Content-Type","application/json").header("Authorization","Bearer eU42yooWtos3e8fVHnFoSIEGb762tSzj9D9c7d9rp2tQyFGfiTJ35QfFaWFNE9ld8ikAk7FPOb2d+AMC7Z7MJg==").header("Accept","application/json")
+						.uri(URI.create(url)).header("Content-Type","application/json").header("Authorization","Bearer MINHA CHAVE").header("Accept","application/json")
 						.POST(HttpRequest.BodyPublishers.ofString(body))
 						.build();
 		HttpResponse<String> resp = client.send(req,HttpResponse.BodyHandlers.ofString());
